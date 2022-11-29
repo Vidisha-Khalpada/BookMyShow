203f7b16a13449a126a4460dc9c75559691c16ab
Luc <68097668+Lucverse@users.noreply.github.com>
2022-11-29 16:09:24 +0530

    Add files via upload

---
+++ scripts/navbar.js
@@ -0,0 +1,9 @@
+
+console.log("yes");
+function openNav() {
+    document.getElementById("mySidebar").style.width = "350px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
