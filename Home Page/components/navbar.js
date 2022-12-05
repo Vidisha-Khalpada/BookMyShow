a69d8ad3a77cf8785c625d43394521894ff7dda5
Abhishek Khanna <113687128+abhishekkhanna2710@users.noreply.github.com>
2022-12-05 15:54:57 +0530

    Update navbar.js

--- Home Page/components/navbar.js
+++ Home Page/components/navbar.js
@@ -1,4 +1,4 @@
-function navbar(){
+function navbar() {
     return `
     <div id="navin">
             <div id="navlogo">
@@ -22,9 +22,47 @@ function navbar(){
                 </div>
                 <div>
                     <button id="navsign" onclick="login()">Sign in</button>
+                    <span id="myBtn2"></span>
+
+                    
+    <div id="myModal" class="modal">
+
+   
+    <div class="modal-content">
+
+
+
+
+        <span class="close">&times;</span>
+        <h3>Get Started</h3>
+
+
+
+        <div class="google">
+            <img src="https://in.bmscdn.com/webin/common/icons/googlelogo.svg" alt="">
+            <p> Continue with Google</p>
+        </div>
+
+        <div class="email">
+            <img src="https://in.bmscdn.com/webin/common/icons/email.svg" alt="">
+            <p> Continue with Email</p>
+        </div>
+        <div class="apple">
+            <i class="fa-brands fa-apple"></i>
+            <p>Continue with Apple</p>
+        </div>
+
+        <p>OR</p>
+
+        <div id="bton">
+            <a href="otp.html"><button id="nos">Create Account</button></a>
+        </div>
+        <br>
+    </div>
+</div>
                 </div>
                 <div id="sidebutton">
-                    <button onclick="openNav()">☰</button>
+                    <button id="styleBAR" onclick="openNav()">☰</button>
                 </div>
             </div>
     </div>`
